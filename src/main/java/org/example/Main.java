bf1b978913abdb91a8fff12be3303850812404a3
Jesse Calhoun <jessecalhoun@Jesses-MacBook-Air.local>
2023-02-23 00:42:34 -0500

    Created 3 children class to the Weapon class, Lightsaber, Spear and Dagger and instantiated them and gave them to a instantiations of the robot class with slightly different printouts to determine the correct line is being printed in the right spots.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,20 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        Weapon sword = new Weapon("Sword", 50);
-        Weapon spear = new Weapon("Spear", 40);
-        Robot robot_one = new Robot(spear);
-        System.out.println(robot_one.name);
+//        Weapon sword = new Weapon("Sword", 50);
+//        Weapon spear = new Weapon("Spear", 40);
+        Lightsaber lightsaber = new Lightsaber();
+        Spear spear = new Spear();
+        Dagger dagger = new Dagger();
+        Robot robot_one = new Robot(lightsaber);
+        System.out.println(robot_one.name + " 1");
         robot_one.Attack();
+        Robot robot_two = new Robot(spear);
+        System.out.println(robot_two.name + " 2");
+        robot_two.Attack();
+        Robot robot_three = new Robot(dagger);
+        System.out.println(robot_three.name + " 3");
+        robot_three.Attack();
+
     }
 }
